4a1dd08445340a5b6ca9ccf7b83a36eb26550503
Victor Anhaya <victoranhaya8@icloud.com>
2024-07-11 09:12:11 -0300

    base

---
+++ API/API/API/Program.cs
@@ -0,0 +1,65 @@
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDataContext>();
+
+var app = builder.Build();
+
+
+app.MapGet("/", () => "Final");
+
+//ENDPOINTS DE CATEGORIA
+//GET: http://localhost:5273/pages/aluno/cadastrar
+app.MapGet("/categoria/listar", ([FromServices] AppDataContext ctx) =>
+{
+    if (ctx.Categorias.Any())
+    {
+        return Results.Ok(ctx.Categorias.ToList());
+    }
+    return Results.NotFound("Nenhuma categoria encontrada");
+});
+
+//POST: http://localhost:5273/pages/imc/cadastrar
+app.MapPost("/categoria/cadastrar", ([FromServices] AppDataContext ctx, [FromBody] Categoria categoria) =>
+{
+    ctx.Categorias.Add(categoria);
+    ctx.SaveChanges();
+    return Results.Created("", categoria);
+});
+
+//ENDPOINTS DE TAREFA
+//GET: http://localhost:5273/pages/imc/listar
+app.MapGet("/tarefas/listar", ([FromServices] AppDataContext ctx) =>
+{
+    if (ctx.Tarefas.Any())
+    {
+        return Results.Ok(ctx.Tarefas.ToList());
+    }
+    return Results.NotFound("Nenhuma tarefa encontrada");
+});
+
+//POST: http://localhost:5273/pages/imc/listarporaluno
+app.MapPost("/tarefas/cadastrar", ([FromServices] AppDataContext ctx, [FromBody] Tarefa tarefa) =>
+{
+    Categoria? categoria = ctx.Categorias.Find(tarefa.CategoriaId);
+    if (categoria == null)
+    {
+        return Results.NotFound("Categoria não encontrada");
+    }
+    tarefa.Categoria = categoria;
+    ctx.Tarefas.Add(tarefa);
+    ctx.SaveChanges();
+    return Results.Created("", tarefa);
+});
+
+//PUT: http://localhost:5273/pages/imc/alterar
+app.MapPut("/tarefas/alterar/{id}", ([FromServices] AppDataContext ctx, [FromRoute] string id) =>
+{
+    //Implementar a alteração do status da tarefa
+});
+
+
+
+app.Run();
